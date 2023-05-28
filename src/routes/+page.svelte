98c53006e160bea5fc1bab549dc297bc73501076
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-05-28 15:07:18 -0400

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	let workaround = setInitialClassState.toString();
+
+	function setInitialClassState() {
+		// does something
+	}
+</script>
+
+<svelte:head>
+	<!-- This causes the new eslint-plugin-svelte to throw a parsing error -->
+	{@html `<script nonce="%sveltekit.nonce%">(${setInitialClassState.toString()})();</script>`}
+
+	<!-- This is the workaround  -->
+	{@html `<script nonce="%sveltekit.nonce%">(` + workaround + `)();</script>`}
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
